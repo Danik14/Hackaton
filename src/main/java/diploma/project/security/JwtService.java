e3749c380f838ca42de985919bd0ae7342dd7a97
Danik <chapagana@gmail.com>
2023-06-19 22:01:16 +0600

    Continue on jwt implementation

--- src/main/java/diploma/project/security/JwtService.java
+++ src/main/java/diploma/project/security/JwtService.java
@@ -1,12 +1,17 @@
 package diploma.project.security;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -29,10 +34,44 @@ public class JwtService {
         return claimsResolver.apply(claims);
     }
 
-    // public String generateToken(Map<String, Object> claims, UserDetails
-    // userDetails) {
+    // Generation of token withot extra claims
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
 
-    // }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUserName(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    // Using method reference of a built in mehtod
+    // to extract a expiration date
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    // Generation of token
+    // For now I use email as a subject and UserDetails interface only has
+    // method called getUsername(), thats why. I will figure it out later
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+        return Jwts
+                .builder()
+                // setting parts of token
+                .setClaims(extraClaims)
+                // using email as a subject as of this moment
+                .setSubject(userDetails.getUsername())
+                // setting when this jwt was created for further validation
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                // using signature to sign a token and settin an algorithm
+                .signWith(getSigInKey(), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
 
     // Gettin all claims (parts) of jwt
     // first it sets the sign key to verify the token
