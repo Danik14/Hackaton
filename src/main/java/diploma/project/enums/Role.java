8a1a81ede0095b4fc150a19d64d7fab9566c21f3
Danik <chapagana@gmail.com>
2023-06-20 20:10:56 +0600

    Add permission enum

---
+++ src/main/java/diploma/project/enums/Role.java
@@ -0,0 +1,40 @@
+package diploma.project.enums;
+
+import static diploma.project.enums.Permission.ADMIN_CREATE;
+import static diploma.project.enums.Permission.ADMIN_DELETE;
+import static diploma.project.enums.Permission.ADMIN_READ;
+import static diploma.project.enums.Permission.ADMIN_UPDATE;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+
+    USER(Collections.emptySet()),
+    ADMIN(
+            Set.of(
+                    ADMIN_READ,
+                    ADMIN_UPDATE,
+                    ADMIN_DELETE,
+                    ADMIN_CREATE));
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        var authorities = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+}
